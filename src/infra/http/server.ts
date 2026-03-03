52032ceab835cefb3788b6ac5ba628dce0925822
Marcelle <marcellealves.dev@gmail.com>
2026-03-03 19:10:11 -0300

    Refactor server configuration to use environment variables and add validation error handling

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,13 +1,35 @@
 import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { env } from '../env'
 
 const server = fastify()
+
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      issues: error.validation,
+    })
+  }
+
+  // Envia o erro para alguma ferramenta de observabilidade (Sentry/DataDog/Grafana)
+  console.error(error)
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
 
 server.register(fastifyCors, { origin: '*' })
 
 server
   .listen({
-    port: 3333,
+    port: env.PORT,
     host: '0.0.0.0',
   })
   .then(() => {
